710f478d19afb946f87e8c51ce0d6ccf94f1f275
adriennemadarang <adriennemadarang@icloud.com>
2023-09-22 17:39:52 -0700

    delete and update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,4 +25,49 @@ module.exports = {
         }
     },
 
+    // Creates a new user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+//    Updates user
+aysnc updateUser(req, res) {
+    try {
+        const user = await User.findByIdAndUpdate(
+            { _id: req.aparms.userId },
+            { $set: req.body },
+            { runValidators: true, new: true} 
+        );
+        if (!user) {
+            return res.status(404).json({ message: "No user found with that ID!" });
+        }
+        res,json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+},
+
+// Deletes a user
+async deleteUser(req, res) {
+    try {
+        const user = await User.findOneAndDelete({ _id: req.params.userId });
+        if (!user){
+            return res.status(404).json({ message: "No user found with that ID!" })
+        }
+        // deletes users thoughts when user is deleted
+        await Thought.deleteMany({ _id: { $in: user.thoughts } });
+        res.json({ message: "User and thoughts have been deleted." })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}, 
+
+// Adds a friend
+async addFriend(req, res){
     
+}
