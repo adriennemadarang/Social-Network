a5b81ec68520e5f58f60a6651ef7e866f8396307
adriennemadarang <adriennemadarang@icloud.com>
2023-09-30 17:54:33 -0700

    added time stamp

---
+++ utils/datesFormat.js
@@ -0,0 +1,62 @@
+const addDate = (date) => {
+    let dateString = date.toString();
+
+    const lastChar = dateString.charAt(dateString.length - 1);
+
+    if (lastChar === '1' && dateString !== '11') {
+        dateString = `${dateString}st`;
+    } else if (lastChar === '2' && dateString !== '12') {
+        dateString = `${dateString}nd`;
+    } else if (lastChar === '3' && dateString !== '13') {
+        dateString = `${dateString}rd`;
+    } else {
+        dateString = `${dateString}th`;
+    }
+
+    return dateString;
+};
+
+module.exports = (
+    timestamp,
+    { monthLength = 'short', dateSuffix = true } = {}
+) => {
+    const month = {
+        0: monthLength === 'short' ? 'Jan' : 'January',
+        1: monthLength === 'short' ? 'Feb' : 'February',
+        2: monthLength === 'short' ? 'Mar' : 'March',
+        3: monthLength === 'short' ? 'Apr' : 'April',
+        4: monthLength === 'short' ? 'May' : 'May',
+        5: monthLength === 'short' ? 'Jun' : 'June',
+        6: monthLength === 'short' ? 'Jul' : 'July',
+        7: monthLength === 'short' ? 'Aug' : 'August',
+        8: monthLength === 'short' ? 'Sep' : 'September',
+        9: monthLength === 'short' ? 'Oct' : 'October',
+        10: monthLength === 'short' ? 'Nov' : 'November',
+        11: monthLength === 'short' ? 'Dec' : 'December',
+    };
+
+    const dateObject = newDate(timestamp);
+    const monthFormat = month[dateObject.getMonth()];
+
+    const day = dateSuffix
+    ? addDateSuffix(dateObject.getDate())
+    : dateObject.getDate();
+
+    const year = dateObject.getFullYear();
+    let hour = 
+    dateObject.getHours() > 12
+    ? Math.floor(dateObject.getHours() - 12)
+    : dateObject.getHours();
+
+    if (hour === 0) {
+        hour = 12;
+    }
+
+    const minutes = (dateObject.getMinutes() < 10 ? '0' : '') + dateObject.getMinutes();
+
+    const timeOfday = dateObject.getHours() >= 12 ? 'pm' : 'am';
+
+    const timestampFormat = `${monthFormat} ${day}, ${year} at ${hour}: ${minutes} ${timeOfday}`;
+
+    return timestampFormat;
+};
